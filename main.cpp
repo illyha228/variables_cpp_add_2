676a1cbcab61981de6740c691591e3a1a7f33f4d
do9m66619 <tdregsdasfsdg@gmail.com>
2024-11-13 18:48:13 +0200

    repleat

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
+
+	int year;
+	cout << "Enter a year: ";
+	cin >> year;
+	int days = 365 + (year % 4 == 0 && year % 100 != 0 || year %  400 == 0);
+	cout << "the Number of days in the year" << year << " is " << days << endl;
 
 	return 0;
 }
